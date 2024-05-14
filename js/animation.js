c5274a43cea6183392ba0b507ffb9783f8f099e0
sridhar_swain <sridhar_swain@intuit.com>
2024-05-14 11:55:57 +0530

    side navigation added

--- js/animation.js
+++ js/animation.js
@@ -1,9 +1,28 @@
-let excalimation=true
+let excalimation = true
 
-setInterval(() => {
-    let excalimationComponent = document.getElementById("exclaimation")
-    excalimationComponent.innerText = excalimation 
-        ? "Hi there"
-        : "Hi there!"
-    excalimation = !excalimation
-}, 500)
+
+
+const onReady = () => {
+   
+}
+
+$(document).ready(function () {
+
+    // Add Navigation
+    $('.dots li').click(function () {
+        console.log("Active")
+        $('.active').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    // Add exclaimation blink
+    setInterval(() => {
+        let excalimationComponent = document.getElementById("exclaimation")
+        excalimationComponent.innerText = excalimation
+            ? "Hi there"
+            : "Hi there!"
+        excalimation = !excalimation
+    }, 500)
+
+
+});
